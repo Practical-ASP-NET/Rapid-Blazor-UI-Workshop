22d22d1422488cc5bd0702d7678a015e7d1c75bf
Jon Hilton <hilton.jon@gmail.com>
2021-10-22 12:26:47 +0100

    More exercises

---
+++ RapidBlazorUI.Client/Exercises/4-ProductList/ProductApi.cs
@@ -0,0 +1,15 @@
+﻿namespace RapidBlazorUI.Client.Exercises._4_ProductList
+{
+    public class ProductApi
+    {
+        public List<Product> Products { get; set; } = new List<Product> { new Product() };
+
+        public class Product
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string ImageUrl { get; set; }
+
+        }
+    }
+}
